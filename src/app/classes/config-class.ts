ab3b9832a272df6a5cf993ea471d32031c022898
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-09 15:24:32 -0300

    Implementando Implementando Listagem de Imagens tabela do CRUD com Bootstrap - Carregando informações no Front End

--- src/app/classes/config-class.ts
+++ src/app/classes/config-class.ts
@@ -2,5 +2,4 @@ export class ConfigClass {
     static getUrlApi() {
         return 'http://localhost:3000';
     }
-
 }
--- src/app/componentes/galeria/manter-galeria/manter-galeria.component.ts
+++ src/app/componentes/galeria/manter-galeria/manter-galeria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { GaleriaService } from '../../../servicos/galeria/galeria.service';
+import { ConfigClass }from '../../../classes/config-class';
 
 @Component({
   selector: 'app-manter-galeria',
@@ -9,11 +10,28 @@ import { GaleriaService } from '../../../servicos/galeria/galeria.service';
 })
 export class ManterGaleriaComponent implements OnInit {
 
+  exibriListagemForm  : boolean = false;
+  listImgGaleria      : any;
+  server              : String  = ConfigClass.getUrlApi().toString();
+
+
   constructor( private galeriService:GaleriaService ) { }
 
   ngOnInit() {
+    this.listar();
+  }
+
+  listar(): void {
     this.galeriService.getTodos().subscribe(resp => {
+      this.listImgGaleria = resp.body.dados.map(function(objeto){
+        return {
+          id_galeria  : objeto.id_galeria,
+          titulo      : objeto.titulo,
+          caminho     : this + (objeto.caminho ? objeto.caminho.substring(1) :objeto.caminho )
+        }
+      }, this.server);
       console.log('resposta', resp);
+      console.log('resposta', this.listImgGaleria);
     });
   }
 
