335c6fcca07eacdfc4a5058952ba63da3c4c6d68
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-09 21:14:31 -0300

    Alerts do Bootstrap e verificação de retorno de requisição HTTP da API

--- src/app/componentes/galeria/manter-galeria/manter-galeria.component.ts
+++ src/app/componentes/galeria/manter-galeria/manter-galeria.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { GaleriaService } from '../../../servicos/galeria/galeria.service';
 import { ConfigClass }from '../../../classes/config-class';
 
@@ -13,7 +12,7 @@ export class ManterGaleriaComponent implements OnInit {
   exibriListagemForm  : boolean = false;
   listImgGaleria      : any;
   server              : String  = ConfigClass.getUrlApi().toString();
-
+  mensagem            : any;
 
   constructor( private galeriService:GaleriaService ) { }
 
@@ -21,18 +20,59 @@ export class ManterGaleriaComponent implements OnInit {
     this.listar();
   }
 
-  listar(): void {
-    this.galeriService.getTodos().subscribe(resp => {
-      this.listImgGaleria = resp.body.dados.map(function(objeto){
-        return {
-          id_galeria  : objeto.id_galeria,
-          titulo      : objeto.titulo,
-          caminho     : this + (objeto.caminho ? objeto.caminho.substring(1) :objeto.caminho )
+  verificarRetornoHttp(resp){
+    if(resp.status >= 400) {
+      this.exibirMsgAlert("Erro ao realizar a requisição", "erro");
+      return true;
+    }
+    else {
+      if(resp.body.erro) {
+        this.exibirMsgAlert(resp.body.msg, "erro");
+        return true;
+      }
+      else {
+        if(resp.body.msg != null) {
+          this.exibirMsgAlert(resp.body.msg, "sucesso");
+          return false;
         }
-      }, this.server);
-      console.log('resposta', resp);
-      console.log('resposta', this.listImgGaleria);
-    });
+      }
+    }
+    return false;
   }
 
+  exibirMsgAlert(msg, tipo) {
+    let dados = "";
+    if(tipo == "sucesso") {
+      dados = `<div classe 'alert alert-success' role='alert'> 
+        <strong>${msg}</strong>
+      </div>`;
+    }
+    else if (tipo == "erro") {
+      dados = `<div class = 'alert alert-danger' role='alert'> 
+        <strong>${msg}</strong>
+      </div>`;
+    }
+    this.mensagem = dados;
+  }
+
+  limparAlert() {
+    this.mensagem = "";
+  }
+
+  listar(): void {
+    this.galeriService.getTodos().subscribe(resp => {
+
+      if (!this.verificarRetornoHttp(resp)) {
+        this.listImgGaleria = resp.body.dados.map(function(objeto){
+          return {
+            id_galeria  : objeto.id_galeria,
+            titulo      : objeto.titulo,
+            caminho     : this + (objeto.caminho ? objeto.caminho.substring(1) :objeto.caminho )
+          }
+        }, this.server);
+        console.log('resposta', resp);
+        console.log('resposta', this.listImgGaleria);
+      }
+    });
+  }
 }
